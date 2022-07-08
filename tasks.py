ff9e92061d7eccdcfce34450cbd1c06c77f2f76f
paras9498 <56954579+paras9498@users.noreply.github.com>
2022-07-08 15:47:56 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -4,6 +4,6 @@ BROKER_URL = 'redis://127.0.0.1:6379/0'
 BACKEND_URL = 'redis://127.0.0.1:6379/0'
 app = Celery('tasks', broker=BROKER_URL, backend=BACKEND_URL, )
 
-@app.task(name='Add two numbers')
+@app.task(name='Add two number')
 def add(x,y):
     return x + y
